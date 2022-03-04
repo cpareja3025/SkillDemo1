761ecbb8527aad3bfb5f2006a02399d940189a75
cpareja3025 <cpareja3025@gmail.com>
2022-03-04 14:31:28 -0800

    Test file

--- SkillDemo1.java
+++ SkillDemo1.java
@@ -1,3 +1,6 @@
 class SkillDemo1 {
+    public static int mult(int x, int y){
+        return (x*y);
+    }
 
 }
---
+++ SkillDemo1Test.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class SkillDemo1Test {
+    @Test
+    public void mult() {
+        assertEquals(5, SkillDemo1.mult(2, 3));
+    }
+    
+}
